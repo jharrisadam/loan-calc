cd6c400d2fff6193acf15cf1c560686eaa08f2ad
Josiah (Si) Harris-Adam <30760329+jharrisadam@users.noreply.github.com>
2017-08-09 13:22:57 -0400

    Add files via upload
    Current revisions, trying to tighten and make the code more efficient.

---
+++ loan2.py
@@ -0,0 +1,14 @@
+payment = float(input("Please enter the monthly payment: "))
+principal = float(input("Please enter the current principal: "))
+interest_rate = float(input("Please enter the interest rate, in decimal form: "))
+year = 365.25
+month = 30
+timeframe = 0
+temp_sum = 0
+
+while principal > 0:
+    timeframe += 1
+    temp_fee = ((principal * int_rate)/365.25)*30
+    principal = (principal + temp_fee) - payment
+
+return timeframe
