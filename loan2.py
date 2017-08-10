c6aee1b45c43bbb251d0f4b3adf44217a150f906
Josiah (Si) Harris-Adam <30760329+jharrisadam@users.noreply.github.com>
2017-08-10 16:43:02 -0400

    Update loan2.py

--- loan2.py
+++ loan2.py
@@ -1,8 +1,6 @@
 payment = float(input("Please enter the monthly payment: "))
 principal = float(input("Please enter the current principal: "))
 interest_rate = float(input("Please enter the interest rate, in decimal form: "))
-year = 365.25
-month = 30
 timeframe = 0
 temp_sum = 0
 
